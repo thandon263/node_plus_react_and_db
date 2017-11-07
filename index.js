91fd121226798b9d22c00ff25823649576dc5503
thandon263 <ncubethando20@gmail.com>
2017-11-07 17:16:17 -0500

    Connecting MongoDB to the application using a mongoDB client Mongoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 // condense it down because we are not returning anything
 // cause it is not assigned to anything.
 require("./services/passport");
+
+// connect to the mongodb database
+mongoose.connect(keys.mongoURI);
 
 // Instantiate the App by creating an Express engine
 const app = express();
