1f2cc815553dab89e0912b93ec7bc765680bf183
thandon263 <ncubethando20@gmail.com>
2017-11-07 15:59:57 -0500

    Working on the backend from ground up

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
+// condense it down because we are not returning anything
+// cause it is not assigned to anything.
+require("./services/passport");
+
+// Instantiate the App by creating an Express engine
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
+// When we require an authRoutes file, it returns a function
+// with an immediately invoked function with the (app) argument.
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT;
-app.listen(PORT || 5000,
-  console.log("Server is running on Port: 5000.")
-);
+app.listen(PORT || 5000, console.log("Server is running on Port: 5000."));
 
 // localhost:5000
