8a22d5a008ef55b265a0477269b6659d62c70ae2
thandon263 <ncubethando20@gmail.com>
2017-11-08 10:57:09 -0500

    Finished authentication with OAuth Google.

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - production key here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOODLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URL,
+  cookieKey: process.env.COOKIE_KEY
+};
